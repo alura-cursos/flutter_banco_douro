ce3e3ead6664bc867ff0c7cd8742e17d97e978b3
ricarthlima <rrsl@cin.ufpe.br>
2024-10-08 14:33:16 -0300

    mnm: textos e imagem no modal

--- lib/ui/widgets/add_account_modal.dart
+++ lib/ui/widgets/add_account_modal.dart
@@ -7,6 +7,35 @@ class AddAccountModal extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height * 0.75,
+      padding: const EdgeInsets.all(32),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(height: 16),
+          Center(
+            child: Image.asset(
+              "assets/images/icon_add_account.png",
+              width: 64,
+            ),
+          ),
+          const SizedBox(height: 32),
+          const Text(
+            "Adicionar nova conta",
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            "Preencha os dados abaixo:",
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
